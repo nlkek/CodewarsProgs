aa00a4a40b5410a2f901073e6ef013d5fd4c87d8
nlkek <33432155+nlkek@users.noreply.github.com>
2017-11-28 02:32:25 +0300

    Create ShortestWord.py

---
+++ ShortestWord.py
@@ -0,0 +1,8 @@
+def find_short(s):
+    return min(list(map(len, s.split(' '))))
+    
+    """
+    x Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+"""
