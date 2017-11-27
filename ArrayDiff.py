abb4f23a04711a79809fd2c4197a2a27fbaaf5e3
nlkek <33432155+nlkek@users.noreply.github.com>
2017-11-28 02:42:22 +0300

    Create ArrayDiff.py

---
+++ ArrayDiff.py
@@ -0,0 +1,17 @@
+def array_diff(a, b):
+    tmp = []
+    for i in range(0, len(a)):
+        if a[i] not in b:
+            tmp.append(a[i])
+    return tmp
+    
+    """
+    Your goal in this kata is to implement an difference function, which subtracts one list from another.
+
+It should remove all values from list a, which are present in list b.
+
+array_diff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+array_diff([1,2,2,2,3],[2]) == [1,3]
+"""
